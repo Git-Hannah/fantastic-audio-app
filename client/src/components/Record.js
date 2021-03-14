c71ffbfd438d5a49760816d1bb17fdc98772c755
Hannah <hannahsenft@gmx.net>
2021-03-14 22:28:36 +0100

    Add styles

--- client/src/components/Record.js
+++ client/src/components/Record.js
@@ -5,9 +5,10 @@ import './Record.css';
 export default class Record extends Component {
     render() {
         return (
-            <div className="App-main">
-                <button>
-                    <p>Hello</p>
+            <div className="record">          
+            
+                <button className="button">
+                    <p>Record</p>
                 </button>
             </div>
         )
