b434e22fe5659d2690350f537c96db2823170a10
Hannah <hannahsenft@gmx.net>
2021-03-15 10:05:46 +0100

    Add Backend-Dummy

---
+++ app.js
@@ -0,0 +1,18 @@
+//Starting point of backend logic:
+
+// const express = require('express');
+
+// const app = express();
+
+// //const audiofiles = require('../client/src/audiofiles.json')
+
+// console.log(audiofiles)
+
+// app.get('/api/', (req, res) => {
+//     res.json(audiofiles)
+// });
+
+
+// app.listen(5555, () => {
+//     console.log('listening on port 5555');
+// });
---
+++ routes/audiofiles.js
@@ -0,0 +1,5 @@
+//Here should be routes to CRUD functionality of audiofiles
+
+//variables
+
+//get-, post-, update-, delete- requests
